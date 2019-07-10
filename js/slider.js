ffdeb8c33afebe52a334a5fe56cb3bb240060cc1
Illia Onishchenko <36299368+ilonikso@users.noreply.github.com>
2019-07-10 21:29:13 +0300

    Slider changing

--- js/slider.js
+++ js/slider.js
@@ -2,21 +2,29 @@ var slide = document.getElementsByClassName('slide'),
     rightArrow = document.getElementsByClassName('rightArrow')[0],
     leftArrow = document.getElementsByClassName('leftArrow')[0];
 
-leftArrow.onclick = function() {
+rightArrow.onclick = function() {
   var temp = slide[0].getAttribute('class');
   
   slide[0].setAttribute('class', slide[1].getAttribute('class'));
   
   slide[1].setAttribute('class', slide[2].getAttribute('class'));
+    
+  slide[2].setAttribute('class', slide[3].getAttribute('class'));    
+    
+  slide[3].setAttribute('class', slide[4].getAttribute('class'));      
   
-  slide[2].setAttribute('class', temp);
+  slide[4].setAttribute('class', temp);
 }
 
-rightArrow.onclick = function() {
+leftArrow.onclick = function() {
   var temp = slide[0].getAttribute('class');
   console.log(temp);
   
-  slide[0].setAttribute('class', slide[2].getAttribute('class'));
+  slide[0].setAttribute('class', slide[4].getAttribute('class'));
+    
+  slide[4].setAttribute('class', slide[3].getAttribute('class'));
+    
+  slide[3].setAttribute('class', slide[2].getAttribute('class'));    
   
   slide[2].setAttribute('class', slide[1].getAttribute('class'));
   
