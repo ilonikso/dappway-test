fced87598dacda4929b45bc13f30e3132a341d13
Illia Onishchenko <36299368+ilonikso@users.noreply.github.com>
2019-07-25 16:00:44 +0300

    More text in typewritter

--- js/type.js
+++ js/type.js
@@ -7,8 +7,24 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(500)
-  .typeString('Resear')
+  .typeString('Research')
   .pauseFor(2500)
+  .deleteAll()
+  .typeString('Design')
+  .pauseFor(2500)
+  .deleteAll()
+.typeString('Development')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('Scaling')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('Testing')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('Support')
+  .pauseFor(2500)
+  
   
   
   .start();
